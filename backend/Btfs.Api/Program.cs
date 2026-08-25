6fa3736c14a6ec1e3ee11f7f68949a3e3500902e
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2026-08-25 12:29:13 -0300

    Fix weather forecast endpoint formatting

--- backend/Btfs.Api/Program.cs
+++ backend/Btfs.Api/Program.cs
@@ -352,5 +352,3 @@ app.MapGet("/weatherforecast", () =>
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
